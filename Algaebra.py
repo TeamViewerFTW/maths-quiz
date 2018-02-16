7113355f0f3a07866ea74c40f1a44d3667170393
TeamViewerFTW <atticusb05@gmail.com>
2018-02-16 10:24:41 +0800

    send

--- Algaebra.py
+++ Algaebra.py
@@ -9,18 +9,22 @@ time.sleep(1.0)
 x = random.randint(1, 30)
 y = random.randint(1, 30)
 a = random.randint(1, 30)
-print ("Evaluate 4x+7y+2a*-3^2".)
-print ("where x equals\n")
+print ("Evaluate 4x+7y+2a*-3^2.")
+print ("where x equals")
 print (x)
-print ("y equals\n")
+print ("y equals")
 print (y)
 print ("and A equals")
 print (a) 
 #   t.start() # starts the timer
 answer_answer = int(input("Answer: "))
+correct_answer = answer
 if answer_answer == -36:
     print ("Yay! Next question: Evaluate 22x*4y where x=10 and y=2.")
  
+    
+elif answer_answer == "":
+    print ("No blanks!")
     
 else:
     print ("Fail!")
