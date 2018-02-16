9c5e8b58b4e4fa616ab776b80bce71c561f038de
TeamViewerFTW <atticusb05@gmail.com>
2018-02-16 10:08:36 +0800

    I broke it!

--- Algaebra.py
+++ Algaebra.py
@@ -7,7 +7,7 @@ time.sleep("1")
 print ("Evaluate 4x+7y+2a*-3^2 where x=1, y=2 and a=3.") # Prints the first question
 #   t.start() # starts the timer
 answer_answer = input("Answer: ")
-if (answer_answer = "-36"):
+if answer_answer = "-36":
     print ("Yay! Next question: Evaluate 22x*4y where x=10 and y=2.")
  
     
