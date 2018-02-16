2dc6bdf5a654b70111c1d7c62a1c4bf7f0f2f9e0
TeamViewerFTW <atticusb05@gmail.com>
2018-02-16 10:15:05 +0800

    More code!

--- Algaebra.py
+++ Algaebra.py
@@ -1,13 +1,19 @@
-from threading import Timer
+import threading
+import time
 import sys
+import random
 print ("Welcome to the Algebra Quiz: Python Edition!")
 print ("Question 1.")
-time.sleep("1")
+time.sleep(1.0)
 #    t = Timer(10.0, answer) # sets up timer
+x = random.randint(1, 30)
+y = random.randint(1, 30)
+a = random.randint(1, 30)
 print ("Evaluate 4x+7y+2a*-3^2 where x=1, y=2 and a=3.") # Prints the first question
 #   t.start() # starts the timer
-answer_answer = input("Answer: ")
-if answer_answer = "-36":
+answer_answer = int(input("Answer: "))
+answer_answer = input
+if answer_answer == -36:
     print ("Yay! Next question: Evaluate 22x*4y where x=10 and y=2.")
  
     
