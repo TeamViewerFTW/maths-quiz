2566ab544708b50dacf77a458d0a88c3b98217de
TeamViewerFTW <atticusb05@gmail.com>
2018-02-23 09:46:16 +0800

    Wut?

--- Algaebra.py
+++ Algaebra.py
@@ -16,10 +16,11 @@ print ("y equals")
 print (y)
 print ("and A equals")
 print (a) 
+answer = 
 #   t.start() # starts the timer
 answer_answer = int(input("Answer: "))
-correct_answer = answer
-if answer_answer == -36:
+correct_answer = (4x+7y+2a*-3^2)
+if answer_answer == correct_answer:
     print ("Yay! Next question: Evaluate 22x*4y where x=10 and y=2.")
  
     
