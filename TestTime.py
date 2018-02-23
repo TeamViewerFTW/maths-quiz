5bab379522f558dbddce196749389bb5385be1a6
TeamViewerFTW <atticusb05@gmail.com>
2018-02-23 10:21:38 +0800

    Edited timer

--- TestTime.py
+++ TestTime.py
@@ -1,7 +1,11 @@
 import time
+from subprocess import call
 mintt=input("How many seconds you want to time?:")
 timer=int(mintt)
 while (timer != 0 ):
     timer=timer-1
     time.sleep(1)
     print(timer)
+    if (timer == 0 ):
+	call (["afplay", "Airhorn.wav"])
+
